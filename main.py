9e6bca0ec3e1404a534e9146b697689a32378b74
rgarcia2304 <rgarcia22141@gmail.com>
2025-08-21 06:37:48 -0400

    Ship now added to screen

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0 
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         last_tick = clock.tick(60)
         dt = last_tick / 1000
